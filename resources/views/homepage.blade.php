41de534881b6d4097dc30009022186b4803f8475
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-06-11 12:32:40 +0200

    ciclo

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,7 +6,12 @@
 <main id="comics">
     <div class="container">
       <div class="product-label">Current Series</div>
-     
+      @foreach ($comic as $comics)
+        <div class="card">
+            <img src="{{$comics['thumb']}}" alt="{{$comics['type']}}">
+            <h2>{{$comics['series']}}</h2>
+        </div>
+        @endforeach
     </div>
     <div class="load-more">
       <div class="btn-main"><span>Load More</span></div>
